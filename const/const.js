18d2ea2ab209a585dda9799f46401214d11f23e9
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-10-25 15:30:34 +0300

    some corrections

--- const/const.js
+++ const/const.js
@@ -1,6 +1,6 @@
 module.exports.urlRule = /(^https?:\/\/)(www.)?[a-z0-9\-\\._~:\\/?#\\[\]@!$&'\\(\\)*\\+,;=]/;
 
 module.exports.allowedCors = [
-  'http://project-movie.nomoredomains.icu',
-  'https://project-movie.nomoredomains.icu',
+  'http://project-movies.nomoredomains.icu',
+  'https://project-movies.nomoredomains.icu',
 ];
