bb1fd2502271a0ef1a267ad4400f2b25d4023c1d
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-10-26 18:33:17 +0300

    before 2 review

--- const/const.js
+++ const/const.js
@@ -15,7 +15,7 @@ module.exports.msgNoUser = 'Запрашиваемый пользователь 
 
 module.exports.msgNotUniqueUser = 'Пользователь с таким email уже существует';
 
-module.exports.msgAuthErr = 'Неверные почта и(или) пароль';
+module.exports.msgAuthErr = 'Неверные почта или пароль';
 
 module.exports.msgNotFound = 'Неверные почта и(или) пароль';
 
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs'); // импортируем bcrypt
 const validator = require('validator');
 const AuthError = require('../errors/auth-error');
+const { msgAuthErr } = require('../const/const');
 
 const userSchema = new mongoose.Schema(
   {
@@ -38,13 +39,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        throw new AuthError('Неправильные почта или пароль');
+        throw new AuthError(msgAuthErr);
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            throw new AuthError('Неправильные почта или пароль');
+            throw new AuthError(msgAuthErr);
           }
           return user; // теперь user доступен
         });
