ab5da20e3ec7e85dc0a47403ac31f79d627a60eb
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-11-28 11:17:37 +0300

    +cors +login after register

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -8,7 +8,8 @@ module.exports = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    // res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Credentials', true);
   }
 
